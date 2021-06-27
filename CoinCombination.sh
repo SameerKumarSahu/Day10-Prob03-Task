e9edb253155484abd76b57e3d291a7141f0a8c77
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 22:03:42 +0530

    Singlet combination

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to the coin combinatin program...."
 
+declare -A Dict
+
 Flipcoin=$((RANDOM%2))
 
 if [ $Flipcoin -eq 0 ]
@@ -10,3 +12,17 @@ then
 else
 	echo "TAIL"
 fi
+
+
+for (( i=1; i<=10; i++ ))
+do
+Flipcoin=$((RANDOM%2))
+
+if [ $Flipcoin -eq 0 ]
+then
+        Dict[Face1]="HEAD"
+else
+        Dict[Face2]="TAIL"
+fi
+done
+echo ${Dict[@]}
